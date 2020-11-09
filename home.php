56b30832cf8ddfe1745fe69d9aef5b184f8839af
samgavin <sureshgopimd12@gmail.com>
2020-11-09 12:50:42 +0530

    install project

---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+	<link rel="stylesheet" href="css\bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
